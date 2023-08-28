c1ffdb5f6410c5b20884518335ae063012fe162e
ying1488 <yingecteach@gmail.com>
2023-08-29 08:40:18 +1000

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+const Runner = require('./runner');
+const runner = new Runner();
+
+const run = async () => {
+    await runner.collectFiles(process.cwd());
+    runner.runTests();
+};
+
+run();
